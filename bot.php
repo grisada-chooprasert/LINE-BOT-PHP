c97ca64d15810f2e912e58e8a8d096b747f93a07
grisada-chooprasert <grisada@truemail.co.th>
2017-08-25 14:08:49 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,6 +1,6 @@
 <?php
  
-$strAccessToken = "ACCESS_TOKEN";
+$strAccessToken = "1MJDwwxqL6hI/7U4ssQc0aJx4fV940rXotq4N3eawDivEZErVfE34K4YfKFnrw2uMz3YvJVad8eEDGlxCWgRKsFiqDnsyiNjGxE1KhTnf6/kZ7Wia//Ioq8pxQeCeFhV76VhiBvW8c9g4ehhr/tjQwdB04t89/1O/w1cDnyilFU=";
  
 $content = file_get_contents('php://input');
 $arrJson = json_decode($content, true);
