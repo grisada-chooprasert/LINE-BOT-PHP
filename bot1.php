ae00f7647024cf622e9ba6049dd2677ed96b02e3
grisada-chooprasert <grisada@truemail.co.th>
2017-08-25 21:09:15 +0700

    Update bot1.php

--- bot1.php
+++ bot1.php
@@ -9,7 +9,7 @@ $arrHeader[] = "Content-Type: application/json";
 $arrHeader[] = "Authorization: Bearer {$strAccessToken}";
  
 $arrPostData = array();
-$arrPostData['to'] = "{UserId}";
+$arrPostData['to'] = "C6ed7b5f7e8390e449c8eef501474c8a7";
 $arrPostData['messages'][0]['type'] = "text";
 $arrPostData['messages'][0]['text'] = "นี้คือการทดสอบ Push Message";
  
