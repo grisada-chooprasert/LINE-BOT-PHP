f9e66da6448ac39da31af6fc741068ed928b19a9
grisada-chooprasert <grisada.chooprasert@gmail.com>
2018-01-27 23:43:17 +0700

    ds

--- bot0.php
+++ bot0.php
@@ -1,6 +1,6 @@
 <?php
 
-$access_token = '1MJDwwxqL6hI/7U4ssQc0aJx4fV940rXotq4N3eawDivEZErVfE34K4YfKFnrw2uMz3YvJVad8eEDGlxCWgRKsFiqDnsyiNjGxE1KhTnf6/kZ7Wia//Ioq8pxQeCeFhV76VhiBvW8c9g4ehhr/tjQwdB04t89/1O/w1cDnyilFU=';
+$access_token = '5zL0CGtyb7cKkanmoP6nnfjV1iFqKWuaUzS5w0Qm/SjYeDCUqlWDQmcWGGleqeUZMz3YvJVad8eEDGlxCWgRKsFiqDnsyiNjGxE1KhTnf682MoHZF9aDeI66vEa55EmMX28CvhlmdNjboo5KcZDT7gdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
